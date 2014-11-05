d9a81c430b44dec3ea2f3ab39eb3794624922116
ZepaMZarp <rafael.bit7@gmail.com>
2014-11-05 20:10:42 -0200

    Update App.java
    Web

--- src/br/com/yaw/git/App.java
+++ src/br/com/yaw/git/App.java
@@ -3,7 +3,7 @@ package br.com.yaw.git;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Utilizando o git pelo NetBeans 8.0");
+        System.out.println("Utilizando o git pelo NetBeans 8.0 novo");
         int n = 30;
     }    
 }
