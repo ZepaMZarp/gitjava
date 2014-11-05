c1daffbea2c54489e40457940693e4f034a8115d
rafael.031195 <rafael.031195@192.168.3.66>
2014-11-05 19:52:49 -0200

    Fazendo Commit d oApp

--- src/br/com/yaw/git/App.java
+++ src/br/com/yaw/git/App.java
@@ -3,6 +3,7 @@ package br.com.yaw.git;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Utilizando o git pelo NetBeans");
+        System.out.println("Utilizando o git pelo NetBeans 8.0");
+        int n = 30;
     }    
 }
